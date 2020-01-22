ac1acd689dd760ffa4bf27ced2f1ee192643309c
Suruchi1986 <suruchis.sharma@gmail.com>
2020-01-22 11:27:29 +0800

    login page fix

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
-router.get('/', function(req, res, next) {
-  
-    res.render('login');
-=======
 router.get('/', function (req, res, next) {
   res.redirect('/auth/login')
->>>>>>> 3a81a34db3a498b0a45ed050d239be55c57ef18f
 });
 
 //FEED
