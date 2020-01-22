ddb82b9cfccf42708736d3ef91027efa178fc595
Suruchi1986 <suruchis.sharma@gmail.com>
2020-01-22 11:22:33 +0800

    login placeholder fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-    res.render('login')
+    res.render('login');
 });
 
 router.post('/login', function(req,res,next){
